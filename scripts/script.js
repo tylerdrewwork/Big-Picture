5c0e97031e077057aa55f23cda5e7ce93cdd03f4
Sakiskid <tylerdrew.home@gmail.com>
2020-08-27 19:19:37 -0500

    starting ajax query work for Adzuna

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+function makeAdzillaQuery(){
+    // https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id={YOUR_APP_ID}&app_key={YOUR_APP_KEY}
+    
+    let URL = "";
+    $.ajax({
+        url: URL,
+        method: GET
+    }).then(function(response) {
+
+    });
+}
