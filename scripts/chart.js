c06e67270acf4a0d0d63df80269d63fc4966292a
Chambers Mansour <chambersmansour97@gmail.com>
2020-08-30 16:13:45 -0500

    added phsyical chart

--- scripts/chart.js
+++ scripts/chart.js
@@ -7,7 +7,7 @@ var myChart = new Chart(ctx, {
     data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-            label: '# of Votes',
+            label: '# of Times Appeared in Searched Articles',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
